d5190b9ed2a3f05891ce3b14303a812aa214bd1a
Assubishi <Khamiyev02@gmail.com>
2022-05-17 17:12:59 +0500

    Online Magazine with API

---
+++ project2/webpack.config.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports = {
+    mode: 'development',
+    entry: ['./public/js/main.js', './public/js/CartComponent.js', './public/js/ErrorComp.js', './public/js/FilterComp.js', './public/js/ProductComponent.js'],
+    output: {
+        filename: './build.js'
+    }
+}
